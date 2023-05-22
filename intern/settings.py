f794cfa1537d2c2a6557906794ce9bf41494f64b
= <=>
2023-05-22 21:06:30 +0900

    login finish

--- intern/settings.py
+++ intern/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'intern.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,9 +119,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media_local'
+
+AUTH_USER_MODEL = "myapp.CustomUser"
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth import login, authenticate
+from.forms import SignUpForm
 
 
 def index(request):
     return render(request, "myapp/index.html")
 
 def signup_view(request):
-    return render(request, "myapp/signup.html")
+    if request.method == 'GET':
+        form = SignUpForm()
+    elif request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password1"]
+            img = form.cleaned_data["img"]
+            user = authenticate(username=username, email=email, password=password, img=img)
+            login(request, user)
+
+            return redirect('index')
+        
+    context = {"form":form}
+    return render(request, "myapp/signup.html", context)
 
 def login_view(request):
     return render(request, "myapp/login.html")
